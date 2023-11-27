8ed2f5551a55303f4a8bce02951802b2d3c83124
Peter <therealphrenologist@gmail.com>
2023-11-27 15:59:27 +0100

    bumped to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blinklab_python_sdk',
-    version='0.1.0',
+    version='0.1.1',
     author='Peter Boele',
     author_email='peter@blinklab.org',
     packages=find_packages(),
