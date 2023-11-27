7aa90296b37e3f4d089b3dd8e4f5937cc14a147c
Peter <therealphrenologist@gmail.com>
2023-11-27 15:42:41 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blinklab_python_sdk',
+    version='0.1.0',
+    author='Peter Boele',
+    author_email='peter@blinklab.org',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy'
+    ],
+    description='Signal processing for Blinklab data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BlinkLab-Pty-Ltd/BlinkLab-python-sdk',
+)
