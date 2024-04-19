48e684d8e1e979cf632700c3309ef6cc74c2787a
Peter <therealphrenologist@gmail.com>
2024-04-19 13:02:13 +0200

    improved outlier detection

--- blinklab_python_sdk/functions.py
+++ blinklab_python_sdk/functions.py
@@ -6,9 +6,8 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-BASELINE_BLINK_THRESHOLD = 0.3
-BASELINE_DETECTION_WINDOW = 3
-TRACE_VALUE_LOWER_THRESHOLD = -0.12
+BASELINE_BLINK_THRESHOLD = 0.2
+BASELINE_DETECTION_WINDOW = 6
 MIN_MAX_LOWER_PERCENTILE = 1
 MIN_MAX_UPPER_PERCENTILE = 99.9
 
@@ -19,7 +18,7 @@ def remove_baseline_blinks(trace, baseline_length, sort_order):
         return None
 
     trace_array = np.array(trace)
-    baseline = trace_array[:baseline_length]
+    baseline = trace_array[:baseline_length + 1]
     window = baseline[-BASELINE_DETECTION_WINDOW:]
 
     for value in window:
@@ -31,7 +30,7 @@ def remove_baseline_blinks(trace, baseline_length, sort_order):
     return trace
 
 
-def remove_traces_below_threshold(trace, sort_order):
+def remove_traces_below_threshold(trace, sort_order, threshold):
     """ Removes traces that contain values below the threshold"""
     if trace is None:
         return None
@@ -39,26 +38,13 @@ def remove_traces_below_threshold(trace, sort_order):
     trace_array = np.array(trace)
 
     for value in trace_array:
-        if value < TRACE_VALUE_LOWER_THRESHOLD:
+        if value < threshold:
             print(
-                f"Trace {sort_order} contains values below {TRACE_VALUE_LOWER_THRESHOLD} "
+                f"Trace {sort_order} contains values below {threshold} "
                 f"({value}) (trace will be filtered out)")
             return None
 
     return trace
-
-
-def has_extreme_outliers(trace, threshold):
-    """ Checks if the trace contains extreme outliers"""
-    if trace is None:
-        return False
-
-    for element in trace:
-        is_outlier = element < threshold
-        if is_outlier:
-            return True
-
-    return False
 
 
 def cleanse_nan_trace(trace):
@@ -97,11 +83,14 @@ def percentile_min_max_scale_trace(trace, global_min, global_max, new_min=0, new
 
 
 def calculate_nan_percentage(trace):
-    """ Calculates the percentage of nan values in a trace"""
+    """ Calculates the percentage of nan values in the first 70% of a trace"""
     if trace is None:
         return 100
 
-    return round(sum(np.isnan(trace)) / len(trace) * 100, 1)
+    cut_off_index = int(len(trace) * 0.70)
+    truncated_trace = trace[:cut_off_index]
+
+    return round(sum(np.isnan(truncated_trace)) / len(truncated_trace) * 100, 1)
 
 
 def baseline_correct_trace(trace, trial_sort_order, baseline_length):
@@ -111,10 +100,9 @@ def baseline_correct_trace(trace, trial_sort_order, baseline_length):
         return None
 
     trace = np.array(trace)
-    trace = trace + 10
-
+    trace = trace + 1
     baseline = trace[:baseline_length]
-    baseline_value = round(np.nanmedian(baseline), 4)
+    baseline_value = np.nanmedian(baseline)
 
     if np.isnan(baseline_value):
         print(f"baseline_correct_trace: {trial_sort_order} baseline_value is nan, returning None")
@@ -189,3 +177,38 @@ def filter_trace(trace):
     b, a = signal.butter(2, 0.5, 'low')
 
     return signal.filtfilt(b, a, trace)
+
+
+def calculate_global_mean_baseline(param, nr_baseline_samples):
+    """ Calculates the global mean baseline of the traces"""
+    valid_traces = [trace for trace in param if trace is not None]
+    if not valid_traces:
+        return None
+
+    valid_traces = [trace[:nr_baseline_samples] for trace in valid_traces]
+
+    concatenated_traces = np.concatenate([trace for trace in valid_traces])
+    return np.nanmean(concatenated_traces)
+
+
+def calculate_global_sd_baseline(param, nr_baseline_samples):
+    """ Calculates the global standard deviation of the traces"""
+    valid_traces = [trace for trace in param if trace is not None]
+    if not valid_traces:
+        return None
+
+    valid_traces = [trace[:nr_baseline_samples] for trace in valid_traces]
+
+    concatenated_traces = np.concatenate([trace for trace in valid_traces])
+    return np.nanstd(concatenated_traces)
+
+
+def get_outlier_threshold(df, column_name, nr_baseline_samples):
+    """ Calculates the outlier threshold based on the global mean and standard deviation"""
+    global_mean_baseline = calculate_global_mean_baseline(df[column_name].dropna().tolist(), nr_baseline_samples)
+    global_mean_sd = calculate_global_sd_baseline(df[column_name].dropna().tolist(), nr_baseline_samples)
+    threshold = global_mean_baseline - (3 * global_mean_sd)
+
+    print(f"Global mean baseline: {global_mean_baseline} (SD={global_mean_sd}), outlier threshold: {threshold}")
+
+    return threshold
--- examples/data_normalization.py
+++ examples/data_normalization.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import sys
 import warnings
 from blinklab_python_sdk import functions as sdk
@@ -12,14 +11,10 @@ def preprocess(data: pd.DataFrame):
     df.sort_values(by=['trial_sortOrder'], inplace=True)
 
     print()
-    print("Processing data...")
-    print("Baseline length: ", BASELINE_LENGTH, " samples", " (", NR_BASELINE_SAMPLES, ")")
-    print("PPI_Y_MIN_EXTREME_OUTLIER: ", PPI_Y_MIN_EXTREME_OUTLIER)
-    print("Number of rows: ", len(df))
+    print("Number of rows: ", len(df), "Baseline length: ", BASELINE_LENGTH, " samples", " (", NR_BASELINE_SAMPLES, ")")
     print()
 
     df['label'] = df['proto_trial_content'].apply(sdk.make_label)
-
     df['eye_trace'] = df['eye_lid_trace_leftEyeTrace'].apply(sdk.split_csv_trace)
     df['eye_trace'] = df['eye_trace'].apply(sdk.to_numeric_list)
     df['eye_trace'] = df['eye_trace'].apply(lambda x: [-1 * item for item in x] if x is not None else None)
@@ -43,9 +38,10 @@ def preprocess(data: pd.DataFrame):
     #####
     # Handle extreme outliers
     #####
-    df['has_extreme_outliers'] = df.apply(
-        lambda row: sdk.has_extreme_outliers(row['eye_trace'], PPI_Y_MIN_EXTREME_OUTLIER), axis=1)
-    df['eye_trace'] = df.apply(lambda row: None if row['has_extreme_outliers'] else row['eye_trace'], axis=1)
+    threshold = sdk.get_outlier_threshold(df, 'eye_trace', NR_BASELINE_SAMPLES)
+    df['eye_trace'] = df.apply(
+        lambda row: sdk.remove_traces_below_threshold(row['eye_trace'], row['trial_sortOrder'], threshold),
+        axis=1)
 
     ######
     # Process eye_trace
@@ -67,12 +63,14 @@ def preprocess(data: pd.DataFrame):
     df['first_baseline_corrected_trace'] = df['first_baseline_corrected_trace'].apply(
         lambda x: x.tolist() if x is not None else None)
 
+    threshold = sdk.get_outlier_threshold(df, 'first_baseline_corrected_trace', NR_BASELINE_SAMPLES)
     df['first_baseline_corrected_trace'] = df.apply(
-        lambda row: sdk.remove_traces_below_threshold(row['first_baseline_corrected_trace'], row['trial_sortOrder']),
+        lambda row: sdk.remove_traces_below_threshold(
+            row['first_baseline_corrected_trace'], row['trial_sortOrder'], threshold),
         axis=1)
 
     ######
-    # Normalization
+    # First Normalization
     ######
     all_traces = df['first_baseline_corrected_trace'].dropna().tolist()
     global_min, global_max = sdk.calculate_global_percentile_min_max(all_traces)
@@ -88,12 +86,28 @@ def preprocess(data: pd.DataFrame):
         axis=1)
 
     df['second_baseline_corrected_trace'] = df.apply(
+        lambda row: sdk.remove_baseline_blinks(
+            row['second_baseline_corrected_trace'], NR_BASELINE_SAMPLES, row['trial_sortOrder']),
+        axis=1)
+
+    threshold = sdk.get_outlier_threshold(df, 'second_baseline_corrected_trace', NR_BASELINE_SAMPLES)
+
+    df['second_baseline_corrected_trace'] = df.apply(
         lambda row: sdk.remove_traces_below_threshold(
-            row['second_baseline_corrected_trace'], row['trial_sortOrder']),
+            row['second_baseline_corrected_trace'], row['trial_sortOrder'], threshold),
         axis=1)
 
     df['second_baseline_corrected_trace'] = df['second_baseline_corrected_trace'].apply(
         lambda x: x.tolist() if x is not None else None)
+
+    ######
+    # Second Normalization
+    ######
+    all_traces = df['second_baseline_corrected_trace'].dropna().tolist()
+    global_min, global_max = sdk.calculate_global_percentile_min_max(all_traces)
+
+    df['second_baseline_corrected_trace'] = df['second_baseline_corrected_trace'].apply(
+        lambda trace: sdk.percentile_min_max_scale_trace(trace, global_min, global_max) if trace is not None else None)
 
     print("Processing done.")
 
@@ -111,7 +125,6 @@ if __name__ == "__main__":
     BASELINE_LENGTH = int(sys.argv[3])
     SYSTEM_LATENCY = int(sys.argv[4])
     NR_BASELINE_SAMPLES = round(BASELINE_LENGTH / 16.66666667)
-    PPI_Y_MIN_EXTREME_OUTLIER = -0.43
 
     full_df = pd.read_csv(input_file)
     processed_df = preprocess(full_df)
